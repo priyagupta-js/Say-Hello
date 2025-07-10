70fb3bc73d9c72d30417cb74843eaf3d9177a973
Priya Gupta <priyagupta052k@gmail.com>
2025-07-10 23:32:35 +0530

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import react from 'react';
-
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
 import SavePost from './Pages/SavePost/SavePost';
+import Profile from './Pages/Profile/Profile';
 
 function App() {
 
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage/>} />
           <Route path='/savedPosts' element={<SavePost/>} />
+          <Route path='/profile' element= {<Profile />} />
         </Routes>
     </Router>
   )
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -3,3 +3,5 @@ const Profile = () => {
 <div>Profile</div>
     );
 }
+
+export default Profile;
