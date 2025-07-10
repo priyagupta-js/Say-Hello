ded8ca92763efe530ce5f740631942c119212b09
Priya Gupta <priyagupta052k@gmail.com>
2025-07-10 23:24:14 +0530

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import react from 'react';
+
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import Homepage from './Pages/Homepage/Homepage';
+import SavePost from './Pages/SavePost/SavePost';
+
 function App() {
 
   return (
-    <>
-      <div>Hello world </div>        
-    </>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/savedPosts' element={<SavePost/>} />
+        </Routes>
+    </Router>
   )
 }
 
---
+++ src/Pages/SavePost/SavePost.jsx
@@ -0,0 +1,7 @@
+const SavePost =() =>{
+    return(
+        <div>Saved Post</div>
+    );
+}
+
+export default SavePost;
