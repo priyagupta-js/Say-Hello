a7a720ebaf3f01313f89490c4b32f17b98256aee
Priya Gupta <priyagupta052k@gmail.com>
2025-07-11 23:52:34 +0530

    Add Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () =>{
+    return(
+        <div className="nav-container">
+            <div className="nav-content">
+                <div className="nav-left">
+
+                </div>
+                <div className="nav-right">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
