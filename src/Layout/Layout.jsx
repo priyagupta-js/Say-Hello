8ea9f83bb03f9bc39345ee0befbc02f29f3f4c12
Priya Gupta <priyagupta052k@gmail.com>
2025-07-16 17:33:20 +0530

    Add Homepage content

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,11 @@
-// import "./Layout.css";
-// const Layout = ({children}) =>{
-//     return(
-//         <div className="main-container">
-//         <div className="">{children}</div>
-//         </div>
-//     );
-// }
+import "./Layout.css";
+const Layout = ({children}) =>{
+    return(
+        <div className="main-container">
+        <div className="">{children}</div>
+        </div>
+    );
+}
 
 
-// export default Layout;
+export default Layout;
