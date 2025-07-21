3528964d2d53bb567862f8151c339d50050bf33d
Priya Gupta <priyagupta052k@gmail.com>
2025-07-21 21:33:42 +0530

    Add Search functionality

--- src/Pages/SavePost/SavePost.jsx
+++ src/Pages/SavePost/SavePost.jsx
@@ -2,16 +2,22 @@ import PostCard from "../../Components/PostCard/PostCard";
 import "../../Components/PostCard/PostCard.css";
 import { PostContext } from "../../PostContext";
 import "./savedPost.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const SavePost = () => {
   const {savedPosts,handleSavePosts,handleUnsavePosts} = useContext(PostContext);
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleInputChange = (e) =>{
+    setSearchQuery(e.target.value.toLowerCase());
+  } 
   return (
     <div className="save-post-container">
       <div className="save-post-wrapper">
         <div className="header-row">
           <h1 className="heading">Saved Posts</h1>
-          <input type="text" className="post-search-btn" placeholder="Search" />
+          <input type="text" className="post-search-btn" placeholder="Search" value= {searchQuery} onChange={handleInputChange} />
         </div>
         <hr className="divider" />
 
