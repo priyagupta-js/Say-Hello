1d5c47764908671f01fa88080bfe1eba257958e7
Priya Gupta <priyagupta052k@gmail.com>
2025-07-29 23:46:48 +0530

    Add backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-// const express = require('express')
-// const app = express()
+const express = require('express')
+const app = express()
 
-// const connectDB = require ('./sayhello');
-// const dotenv = require('dotenv');
-// const users = require('./src/Routes/routes')
+const connectDB = require ('./sayhello');
+const dotenv = require('dotenv');
+const users = require('./src/Routes/routes')
 
 
-// dotenv.config()
+dotenv.config()
 
-// const PORT = process.env.PORT;
-// app.use(express.json());
+const PORT = process.env.PORT;
+app.use(express.json());
 
-// app.get('/',(req,res)=>{
-//     console.log("Setup is running");
-//     res.send("Setup successful");
-// })
+app.get('/',(req,res)=>{
+    console.log("Setup is running");
+    res.send("Setup successful");
+})
 
-// app.listen(PORT , () =>{
-//     console.log(`App is listenting at port ${PORT}`);
-// })
+app.listen(PORT , () =>{
+    console.log(`App is listenting at port ${PORT}`);
+})
