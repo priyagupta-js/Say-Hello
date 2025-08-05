af3d1c007ad933fdf3c326bae6f3898ed55fd840
Priya Gupta <priyagupta052k@gmail.com>
2025-08-05 17:05:24 +0530

    User-post functionality

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -7,173 +7,85 @@ import { IoClose } from "react-icons/io5";
 import { AiFillClockCircle } from "react-icons/ai";
 import { useState } from "react";
 
-const CreatePost = ({ onExpand, isPopup, onClose }) => {
-  // const [postText, setPostText] = useState("");
- 
-  // const [onClose , setOnClose] = useState("");
+const CreatePost = ({ onExpand, isPopup, onClose ,onSubmit}) => {
+
+  const[postText, setPostText] = useState("");
+
+  const handlePost = () =>{
+    if(postText.trim() === "") return;
+
+    onSubmit(postText.trim());
+
+    setPostText("");
+
+    if (onClose) onClose();
+  };
   return (
-    <>
-        <div className=" bg-white rounded-xl shadow-2xl w-full my-6 p-4 max-w-2xl">
-          {/* <div className="bg-white rounded-xl"> */}
-          <div className="flex justify-between items-center gap-2 my-2">
+    // when isPopup is true then style the Modal otherwise the normal input
+        <div className={`bg-white rounded-xl shadow-2xl w-full relative ${isPopup? "max-w-xl p-6" : "p-4 my-6 max-w-2xl"
+        }`}
+        >
+      {/* close button for modal */}
+      {isPopup && (
+        <button onClick={onClose}
+        className="absolute top-4 right-4 text-gray-500 hover:text-black">
+          <IoClose size={24}/>
+        </button>
+      )}
+
+      {/* Top=section: Profile + textarea */}
+
+          <div className="flex items-center gap-3 mb-4">
             <img
               src={userPic}
-              className="w-10 h-10 rounded-full object-cover"
+              className="w-11 h-11 rounded-full object-cover"
               alt="user"
             />
-            <input
+            </div>
+            
+            {/* textarea */}
+
+            {isPopup ? (
+              <textarea
+              type="text"
+              value={postText}
+              onChange={(e) => setPostText(e.target.value)}
+              placeholder="Share your thoughts!"
+            rows="6"
+            className="w-full text-lg resize-none outline-none"
+            
+          />
+            ):(
+              <input
               type="text"
               onFocus={onExpand}
-              className="flex-1 hover:bg-gray-200 border border-gray-300 cursor-pointer outline-none rounded-full p-3 transition"
+              className="flex-1 hover:bg-gray-200 border border-gray-300 cursor-pointer outline-none rounded-full p-3 transition w-full"
               placeholder="Write a post"
             />
-          </div>
-          <div className="flex justify-around items-center gap-20">
-            <button>
-              <FaVideo size={28} />{" "}
+            )}
+            {/* media buttons */}
+          <div className="flex gap-2 mt-4">
+            <button className="p-2 rounded hover:bg-gray-100"> 
+              <FaVideo size={24} />
             </button>
-            <button>
-              <MdInsertPhoto size={28} />
+            <button className="p-2 rounded hover:bg-gray-100" >
+              <MdInsertPhoto size={24} />
             </button>
-            <button>
-              <FaCamera size={28} />
-            </button>
-          </div>
-        </div>
-      {Expanded && (
-        <div className="bg-white rounded-xl shadow-2xl w-full max-w-2xl my-5 p-5 z-40">
-          <div className="flex items-center gap-75">
-            <div className="flex gap-2 items-center ">
-              <img
-                src={userPic}
-                className="w-10 h-10 rounded-full object-cover"
-                alt="user"
-              />
-              <p className="font-semibold">Priya Gupta</p>
-            </div>
-            <div className="">
-              <IoClose size={20} />
-            </div>
-          </div>
-          <textarea
-            type="text"
-            rows="4"
-            onChange={(e) => {
-              console.log(e.target.value);
-            }}
-            className="border-none w-full resize-none outline-none"
-            placeholder="Share your thoughts!"
-          />
-          {/* media icons */}
-          <div className="flex items-center gap-5 my-4">
-            <button>
-              <FaVideo size={28} />{" "}
-            </button>
-            <button>
-              <MdInsertPhoto size={28} />
-            </button>
-            <button>
-              <FaCamera size={28} />
+            <button className="p-2 rounded hover:bg-gray-100">
+              <FaCamera size={24} />
             </button>
           </div>
-          <hr className="" />
-          <div className="flex justify-end items-center gap-2 my-2">
-            <button>
-              <AiFillClockCircle size={20} />
-            </button>
-            <button className="disabled">Post</button>
+          {/* post button */}
+          {isPopup && (
+            <div className="flex justify-end mt-6">
+            <button
+            disabled={postText.trim() ===""}
+            onClick={handlePost}
+            className={`px-4 py-1.5 text-sm rounded-full font-medium ${postText.trim() ==="" ? "bg-gray-300 cursor-not-allowed":"bg-blue-600 text-white hover:bg-blue-700" }`}
+            >Post</button>
           </div>
-        </div>
       )}
-      {/* popped up - modal  */}
-
-      {/* </div> */}
-    </>
-    //   <div
-    //     className={`bg-white rounded-2xl shadow-2xl w-full relative ${
-    //       isPopup ? "max-w-xl p-6 z-50" : "my-6 p-4 px-5 max-w-2xl"
-    //     }`}
-    //   >
-    //     {/* --- Close Button for Popup --- */}
-    //     {isPopup && (
-    //       <button
-    //         onClick={onClose}
-    //         className="absolute top-4 right-4 text-gray-500 hover:text-black"
-    //       >
-    //         <IoClose size={24} />
-    //       </button>
-    //     )}
-
-    //     {/* --- Top Section: Profile + Privacy Dropdown --- */}
-    //     {isPopup && (
-    //       <div className="flex items-center gap-3 mb-4">
-    //         <img src={userPic} className="w-10 h-10 rounded-full" alt="user" />
-    //         <div>
-    //           <p className="font-semibold">Priya Gupta</p>
-    //         </div>
-    //       </div>
-    //     )}
-
-    //   {/* Text input */}
-    //     {isPopup ? (
-    //       <textarea
-    //         rows={4}
-    //         className="w-full resize-none outline-none text-xl placeholder-gray-500"
-    //         placeholder="Share your thoughts"
-    //       ></textarea>
-    //     ) : (
-    //       <div className="flex items-center space-x-4">
-    //         <img
-    //           src={userPic}
-    //           className="w-12 h-12 rounded-full object-cover"
-    //           alt="user"
-    //         />
-    //         <input
-    //           type="text"
-    //           onFocus={onExpand}
-    //           className="flex-1 hover:bg-gray-200 border border-gray-300 cursor-pointer outline-none rounded-full p-3 transition"
-    //           placeholder="Write a post"
-    //         />
-    //       </div>
-    //     )}
-    //     {/* --- Media Buttons) --- */}
-    //     <div
-    //       className={`flex gap-2 text-gray-600 mt-4 ${
-    //         isPopup ? "text-lg" : "justify-between text-sm my-2"
-    //       }`}
-    //     >
-    //       <button className="flex items-center gap-2 hover:bg-gray-100 p-2 rounded-md">
-    //         <MdInsertPhoto size={24} />
-    //         {isPopup && "Photo"}
-    //       </button>
-    //       <button className="flex items-center gap-2 hover:bg-gray-100 p-2 rounded-md">
-    //         <FaVideo size={22} />
-    //         {isPopup && "Video"}
-    //       </button>
-    //       <button className="flex items-center gap-2 hover:bg-gray-100 p-2 rounded-md">
-    //         <FaCamera size={22} />
-    //         {isPopup && "Activity"}
-    //       </button>
-    //     </div>
-
-    //     {/* --- Post Button (popup only) --- */}
-    //     {isPopup && (
-    //       <div className="flex justify-end mt-6">
-    //         <button
-    //         disabled = {postText.trim() === ""}
-    //         className={`px-4 py-2 rounded-md text-white ${
-    //   postText.trim() === ""
-    //     ? "bg-gray-400 cursor-not-allowed"
-    //     : "bg-blue-600 hover:bg-blue-700"
-    // }`}
-    //           onClick={ () => {setPostText("");
-    //           }}
-    //         >
-    //           Post
-    //         </button>
-    //       </div>
-    //     )}
-    //   </div>
+      </div>
   );
 };
 
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Homepage.css";
+import userPic from "../../assets/user-pic.jpg";
 import { users } from "../../data/data";
 import PostCard from "../../Components/PostCard/PostCard";
 import CreatePost from "../../Components/CreatePost/CreatePost";
@@ -8,52 +9,53 @@ import CreatePost from "../../Components/CreatePost/CreatePost";
 const Homepage = () => {
 
    const [onExpand, setOnExpand] = useState(false);
+   const [userPost , setUserPost] = useState([]);
+
+   const handleNewPost =(text) =>{
+    const newPost = {
+      id:Date.now(),
+      name:"Priya Gupta",
+      desc:text,
+      profile:{userPic},
+      time:"Just now",
+    };
+
+    setUserPost((prev) => [newPost, ...prev]);
+   };
   return (
 
      <>
      {/* collapsed Create Post */}
-     {!onExpanded && 
+     {!onExpand && 
      (
    <CreatePost onExpand={() => setOnExpand(true)}/>
      )}
    
 
-      {/* users-posts */}
+     
+{/* popped -model */}
+        {onExpand && (
+          <>
+          <div className="fixed inset-0 backdrop-blur-sm bg-opacity-30 z-40" onClick={()=> setOnExpand(false)}></div>
+          {/* modal container */}
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <CreatePost isPopup={true} onClose={() => setOnExpand(false)} onSubmit ={handleNewPost}/>
+          </div>
+          </>
+        )}
+
+        {/* dynamic user posts first */}
+
+        {userPost.map((post) =>(
+<PostCard key={PostCard.id} post={post} isSavedPage={false}/>
+        ))}
+        {/* staticPosts */}
+         {/* users-posts */}
       {users.map((post)=>(
         
         <PostCard key={post.id} post= {post} isSavedPage={false}/> ))}
 
-{/* popped -model */}
-        {onExpand && (
-          <>
-          <div className="fixed inset-0 backdrop-blur-sm bg-opacity-30 z-40"> </div>
-          <div className="fixed inset-0 z-50 flex items-center justify-center">
-          <CreatePost />
-          </div>
-          </>
-        )}
-     
     </>
-    // <div className="relative">
-    // {!isExpanded && <CreatePost onExpand = {() => setIsExpanded(true)}/>}
-    //   {users.map((post) => (
-    //     <PostCard key={post.id} post={post} isSavedPage={false} />
-    //   ))}
-    //   {
-    //     isExpanded && (
-    //       <>
-    //         <div className="fixed inset-0 backdrop-blur-xs z-40"
-    //         onClick={ () => setIsExpanded(false)}
-    //         >
-    //         </div>
-
-    //         <div className="fixed inset-0 z-50 flex items-center justify-center">
-    //         <CreatePost isPopup={true} onClose={() => setIsExpanded(false)} />
-    //     </div>
-    //     </>
-    //     )}
-    // </div>
-   
   );
 };
 
